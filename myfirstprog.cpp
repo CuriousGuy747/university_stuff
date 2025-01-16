ec15bb9f2e0fc2b35b2019325ae1d813d18a86a6
CuriosGuy747 <s.shamsiddinov@newuu.uz>
2025-01-16 14:42:12 +0500

    new commit

---
+++ myfirstprog.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() {
+  cout<<"Hello World!\n"<<endl;
+  int a;
+  int b = 13;
+
+  //some code here
+  // cout - console output
+  // cin - console input
+  cin>>a;
+    cout<< a + b <<endl;
+
+  int age;
+  cout <<"Enter your age, please: "<<endl;
+  cin >> age;
+  if(age>=18) {
+    cout<<"You can do some legal stuff"<<endl;
+  }else if(age<=10) {
+    cout<<"Sit at home, kid"<<endl;
+  }
+  cout << "Your age is: "<<age<<endl;
+  return 0;
+  }
+
