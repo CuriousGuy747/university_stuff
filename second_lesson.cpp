a109b76ea67ca4192542fb7d6a1a0390f37ac694
CuriosGuy747 <s.shamsiddinov@newuu.uz>
2025-01-23 14:43:20 +0500

    second lesson

--- second_lesson.cpp
+++ second_lesson.cpp
@@ -4,34 +4,34 @@ using namespace std;
 int main() {
 
   // problem1
-  // string name;
-  // int age;
-  //
-  // cout<<"Please input your name: ";
-  // cin>>name;
-  //
-  // cout<<"Please input your age: ";;
-  // cin>>age;
-  //
-  // cout<<"Welcome "<<name<< " your age is "<<age<<endl;
+  string name;
+  int age;
+
+  cout<<"Please input your name: ";
+  cin>>name;
+
+  cout<<"Please input your age: ";;
+  cin>>age;
+
+  cout<<"Welcome "<<name<< " your age is "<<age<<endl;
 
   // problem2
-  // for (int i = 1; i <= 5; ++i) {
-  //   std::cout << std::right << std::setw(5) << i;
-  // }
+  for (int i = 1; i <= 5; ++i) {
+    std::cout << std::right << std::setw(5) << i;
+  }
 
   // problem3
-  // int a;
-  // int b;
-  // cout<<"Enter a number for a: ";
-  // cin>>a;
-  //
-  // cout<<"Enter a number for b: ";
-  // cin>>b;
-  // int c = a;
-  // a =b;
-  // b = c;
-  // cout<<"Number a: "<<a<< " Number b: "<<b<<endl;
+  int a;
+  int b;
+  cout<<"Enter a number for a: ";
+  cin>>a;
+
+  cout<<"Enter a number for b: ";
+  cin>>b;
+  int c = a;
+  a =b;
+  b = c;
+  cout<<"Number a: "<<a<< " Number b: "<<b<<endl;
 
   // problem4
   int main_int;
@@ -43,10 +43,10 @@ int main() {
   }
 
   // problem5
-  // string text;
-  // cout<<"Enter text: ";
-  // cin>>text;
-  // int padding = (50 - text.length()) / 2;
-  //
-  // std::cout<<std::setw(padding + text.length())<<text<<std::setw(padding + text.length())<<std::endl;
+  string text;
+  cout<<"Enter text: ";
+  cin>>text;
+  int padding = (50 - text.length()) / 2;
+
+  std::cout<<std::setw(padding + text.length())<<text<<std::setw(padding + text.length())<<std::endl;
 }
