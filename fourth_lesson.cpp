23c8fe77d5e75801908d494c26c3351e30569d71
CuriousGuy747 <s.shamsiddinov@newuu.uz>
2025-02-06 15:30:49 +0500

    Add files via upload

---
+++ fourth_lesson.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+using namespace std;
+int main() {
+//  problem1
+// int number;
+// cout << "Enter a number: ";
+// cin >> number;
+// if (number < 0) {
+//   cout << "The number is negative." << endl;
+// }else if (number > 0){
+//   cout << "The number is positive" << endl;
+// }else if (number == 0){
+//   cout << "The number is equal to 0." << endl;
+// }else {
+//   cout << "The number must be in int form" << endl;
+// }
+
+  // problem2
+  // int number2;
+  // cout << "Enter a number: ";
+  // cin >> number2;
+  // if (number2 % 2 == 0) {
+  //   cout << "Number is even" << endl;
+  // }else if (number2 % 2 == 1 || number2 % 2 == -1) {
+  //   cout << "Number is odd" << endl;
+  // }
+
+  // problem3
+  // int num1, num2;
+  // cout << "Enter two numbers with space between: ";
+  // cin >> num1 >> num2;
+  // if (num1 > num2) {
+  //   cout << num1 << " is greater than " << num2 << endl;
+  //   cout << "Largest number is " << num1 << endl;
+  // }else if (num1 < num2) {
+  //   cout << num1 << " is less than " << num2 << endl;
+  //   cout << "Largest number is " << num2 << endl;
+  // }
+
+  // problem4
+  // int num1, num2, num3;
+  // cout << "Enter three numbers with space between: ";
+  // cin >> num1 >> num2 >> num3;
+  // if (num1 > num2 && num1 > num3) {
+  //   cout << "The largest number is " << num1 << endl;
+  // }else if (num2 > num1 && num2 > num3) {
+  //   cout << "The largest number is " << num2 << endl;
+  // }else if (num3 > num1 && num3 > num2) {
+  //   cout << "The largest number is " << num3 << endl;
+  // }
+
+  // problem5
+  // int year;
+  // cout << "Please enter a year: ";
+  // cin >> year;
+  // if (year % 4 == 0 ) {
+  //   cout << year << " is a leap year" << endl;
+  // }else {
+  //   cout << year << " is not a leap year" << endl;
+  // }
+
+  // problem6
+  // float speed;
+  // cout << "Enter speed: ";
+  // cin >> speed;
+  // if (speed <= 20) {
+  //   cout << "Too slow. sheesh." << endl;
+  // }else if (speed >= 90) {
+  //   cout << "Too fast. sheesh." << endl;
+  // }else {
+  //   cout <<"Just right"<< endl;
+  // }
+
+  // problem7
+  // int mark;
+  // cout << "Enter a number: ";
+  // cin >> mark;
+  // if (mark >= 0 && mark <= 100) {
+  //   if (mark >= 50) {
+  //     cout << "Good job boi!!!"<<endl;
+  //     cout << "PASS" << endl;
+  //   }else if (mark < 50) {
+  //     cout << "Bad job boi!!!"<<endl;
+  //     cout << "FAIL" << endl;
+  //   }
+  // }else {
+  //   cout << "Cheating is bad boi!!!"<<endl;
+  // }
+
+  // problem8
+  // int num8;
+  // cout << "Enter number: ";
+  // cin >> num8;
+  // if (num8 < 0) {
+  //   cout << "Negative number!" << endl;
+  // }else if (num8 > 0) {
+  //   cout << "Positive number!" << endl;
+  // }else if (num8 == 0) {
+  //   cout << "Zero!" << endl;
+  // }
+
+  // problem9
+  // char character;
+  // cout << "Enter a character: ";
+  // cin >> character;
+  //   switch (character) {
+  //     case 'g':
+  //       cout << "Go"<<endl;
+  //     break;
+  //     case 'y':
+  //       cout << "Get ready"<<endl;
+  //     break;
+  //     case 'r':
+  //       cout << "Stop"<<endl;
+  //     break;
+  //     default: cout << "Wrong input"<<endl;
+  //   }
+
+  // problem10
+  // int grade;
+  // cout << "Please enter your grade: ";
+  // cin >> grade;
+  // if (grade >= 0 && grade <= 100) {
+  //   if (grade >= 90 && grade <= 100) {
+  //     cout << "Your grade is A" << endl;
+  //   }else if (grade >= 80 && grade <= 89) {
+  //     cout << "Your grade is B" << endl;
+  //   }else if (grade >= 70 && grade <= 79) {
+  //     cout << "Your grade is C" << endl;
+  //   }else if (grade >= 60 && grade <= 69) {
+  //     cout << "Your grade is D" << endl;
+  //   }else if (grade >= 0 && grade <= 59) {
+  //     cout << "Your grade is F" << endl;
+  //   }
+  // }else {
+  //   cout << "Your grade is invalid" << endl;
+  // }
+
+  // problem11
+
+}
